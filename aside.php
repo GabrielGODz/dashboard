d4695ac47a9ecf22baf335c36b65f280d53d11d1
Gabriel <zgabrielvitorino@gmail.com>
2024-04-17 22:16:59 -0300

    Aula_17_04

--- aside.php
+++ aside.php
@@ -1,7 +1,7 @@
 <aside id="asideMenu" class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <img src="<?php echo caminhoURL;?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+  <a href="<?php echo caminhoURL; ?>" class="brand-link">
+    <img src="<?php echo caminhoURL;?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Senac SS</span>
   </a>
 
--- verificar-autenticidade.php
+++ verificar-autenticidade.php
@@ -3,7 +3,7 @@
 session_start();
 
 // CAMINHO FIXO DO SOFTWARE WEB
-define("caminhoURL", "http://localhost/gabriel/dashboard/");
+define("caminhoURL", "http://localhost/gabriel/dashboard");
 
 // VERIFICA SE O USUÁRIO NÃO ESTÁ CONECTADO
 if ($_SESSION["autenticado"] != true) {
@@ -19,15 +19,12 @@ if ($_SESSION["autenticado"] != true) {
 
     // VERIFICAR TEMPO INATIVO DO USUÁRIO
     if (($tempo_atual - $_SESSION["tempo_login"]) > $tempo_limite) {
-        // DESTRUIR QUALQUER SESSÃO EXISTENTE
-        session_destroy();
+        $_SESSION["title"] = "Ops!";
+        $_SESSION["tipo"] = "warning";
+        $_SESSION["msg"] = "Tempo de sessão esgotado!";
 
-        echo "
-        <script>
-            alert('Tempo de sessão esgotado!');
-            window.location='./login.php';
-        </script>
-        ";
+        header ("Location: ./login.php");
+
         exit;
     } else {
         $_SESSION["tempo_login"] = time();
