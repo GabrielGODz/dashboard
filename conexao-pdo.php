dd5ee93f842a77e2c42510203dc981177e01aaea
Gabriel <zgabrielvitorino@gmail.com>
2024-05-08 22:14:18 -0300

    Aula_08_05

--- conexao-pdo.php
+++ conexao-pdo.php
@@ -1,10 +1,10 @@
 <?php
 
-define('username', 'root');
-define('password', '');
+define('username', 'if0_36511511');
+define('password', 'Senac2024');
 
 try {
-    $conn = new PDO('mysql: host=localhost; dbname=ordem_servico', username, password);
+    $conn = new PDO('mysql:host=sql307.infinityfree.com; dbname=if0_36511511_senac', username, password);
 }catch(PDOException $e){
     echo "Error: ". $e -> getMessage();
     exit;
